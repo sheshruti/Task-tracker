8637f32e1cef46b768f55339db66c316f41ba870
sheshruti <royshruti85@gmail.com>
2022-03-23 16:10:37 +0530

    made some minor changes

--- Task-tracker/src/App.js
+++ Task-tracker/src/App.js
@@ -20,13 +20,13 @@ function App() {
  const fetchTasks = async () => {
   const res = await fetch('http://localhost:5000/tasks')
   const data = await res.json();
-  console.log(data);
+ 
    return data ; 
 }
 const fetchTask = async (id) => {
   const res = await fetch(`http://localhost:5000/tasks/${id}`)
   const data = await res.json();
-  console.log(data);
+
    return data ; 
 }  
 //delete 
